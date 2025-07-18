97c82b95767f2ea2fd601df6aaca6f4e990150bc
Max Base <MaxBaseCode@gmail.com>
2025-07-18 19:33:30 +0330

    save

--- main.go
+++ main.go
@@ -61,6 +61,10 @@ func NewApp(config Config) (*App, error) {
 		return nil, fmt.Errorf("failed to load sent post IDs: %w", err)
 	}
 
+	client.Transport = &http.Transport{
+		ForceAttemptHTTP2: false,
+	}
+
 	return &App{
 		client:  client,
 		config:  config,
@@ -154,9 +158,9 @@ func (app *App) setRequestHeaders(req *http.Request, referer string) {
 		"User-Agent":                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
 		"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Accept-Language":           "en-US,en;q=0.5",
-		"Accept-Encoding":           "gzip, deflate, br, zstd",
+		"Accept-Encoding":           "gzip, deflate",
 		"Upgrade-Insecure-Requests": "1",
-		"Connection":                "keep-alive",
+		"Connection":                "close",
 		"Sec-Fetch-Dest":            "document",
 		"Sec-Fetch-Mode":            "navigate",
 		"Sec-Fetch-Site":            "none",
@@ -298,6 +302,7 @@ func (app *App) downloadImage(url, filename string, refererURL string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create request for %s: %w", url, err)
 	}
+	req.Header.Del("Range")
 
 	app.setRequestHeaders(req, refererURL)
 
